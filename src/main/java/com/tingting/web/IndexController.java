7bb4cbb87a60302a310b18f33dd9e786d5bfa5be
Koonayoung518 <78741410+Koonayoung518@users.noreply.github.com>
2021-07-15 13:52:00 +0900

    first commit

---
+++ src/main/java/com/tingting/web/IndexController.java
@@ -0,0 +1,41 @@
+package com.tingting.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String login(){
+        return "login";
+    }
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+    @GetMapping("/main")
+    public String main(){
+        return "main";
+    }
+    @GetMapping("/random")
+    public String random(){
+        return "random";
+    }
+    @GetMapping("/select")
+    public String select(){
+        return "select";
+    }
+    @GetMapping("/open")
+    public String open(){
+        return "open";
+    }
+
+
+
+}
